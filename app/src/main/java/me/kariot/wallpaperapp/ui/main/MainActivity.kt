5438a1f9ec7d3e84951a6df910e3bba00580aa8f
Sreehari K <sreeharikariot@gmail.com>
2021-07-19 23:03:42 +0400

    basic setup

---
+++ app/src/main/java/me/kariot/wallpaperapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.kariot.wallpaperapp.ui.main
+
+import android.os.Bundle
+import me.kariot.wallpaperapp.databinding.ActivityMainBinding
+import me.kariot.wallpaperapp.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
---
+++ app/src/main/java/me/kariot/wallpaperapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package me.kariot.wallpaperapp.utils
+
+import me.kariot.wallpaperapp.BuildConfig
+
+object Constants {
+    val BASE_URL = "https://api.pexels.com/v1/"
+    val API_KEY = BuildConfig.API_KEY
+}
